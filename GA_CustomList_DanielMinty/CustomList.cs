1702c68a10702370ef81dc288d3a57988be97613
Daniel Minty <60455996+Dminty87@users.noreply.github.com>
2024-01-07 20:53:15 -0800

    Add project files.

---
+++ GA_CustomList_DanielMinty/CustomList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_CustomList_DanielMinty
+{
+    internal class CustomList<T>
+    {
+        //Attributes
+        private T[] items;
+        private int count;
+        public int Count => count;
+
+        //Constructors
+        public CustomList() : this(10) { }
+
+        public CustomList(int initialSize)
+        {
+            items = new T[initialSize];
+        }
+
+        //Methods
+        public void Add(T item)
+        {//Add the item to the end of the list and increment count.
+            CheckIntegrity();
+            items[count++] = item;
+        }
+
+        public void AddAtIndex(T item, int index)
+        {
+            CheckIntegrity();
+            for (int i = count - 1; i >= index; i--)
+            {//from the bottom up, move each item down until the desired index is reached
+                items[i + 1] = items[i];
+            }
+            //insert the item at the desired index
+            items[index] = item;
+            //increment count
+            count++;
+        }
+        
+        public bool Remove(T item)
+        {//Remove the first occurance of specified item.
+            for (int i = 0; i < count; i++)
+            {//For each item in the list,
+                if (items[i].Equals(item))
+                {//check if the current item is the desired item.
+                    //if the item matches,
+                    for (i++; i < count; i++)
+                    {//move each item below it one space toward the beginning overriding the previous positions.
+                        items[i - 1] = items[i];
+                    }
+                    //one less item means decrement count
+                    count--;
+                    //job done, return true.
+                    return true;
+                }
+                //if the item doesn't match, keep searching.
+            }
+            //if the item isn't found, return false.
+            return false;
+        }
+
+        private void CheckIntegrity()
+        {//If the list is 80% full, double it's maximum capacity.
+            if (count >= 0.8 * items.Length)
+            {
+                T[] largerArray = new T[items.Length * 2];
+                Array.Copy(items, largerArray, count);
+                items = largerArray;
+            }
+        }
+
+        public T GetItem(int index)
+        {
+            if (index < 0 || index >= count)
+            {//If the index is out of bounds, throw exeption.
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            //in bounds, return value at index
+            return items[index];
+        }
+
+        public T this[int index]
+        {//check if index is out of bounds, then get or set if valid
+            get
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                return items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+                items[index] = value;
+            }
+        }
+
+        public T[] ToArray()
+        {//Return an array that contains only the counted values
+            T[] result = new T[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = items[i];
+            }
+            return result;
+        }
+
+    }//End of CustomList
+}//End of Namespace
---
+++ GA_CustomList_DanielMinty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_CustomList_DanielMinty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomList<double> list = new CustomList<double>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            list.AddAtIndex(1.5, 1);
+            list.AddAtIndex(3.5, 4);
+            list.AddAtIndex(5.5, 7);
+
+            list.Remove(4);
+            list.Remove(2);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            //Test CheckIntegrity()
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
